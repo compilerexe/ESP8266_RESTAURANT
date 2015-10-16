f71b3ca2c55916ec8f69422560a1c8de10252dfc
Compiler Exe <compilerexe@gmail.com>
2015-10-16 11:39:44 +0700

    no message

---
+++ config.js
@@ -0,0 +1,6 @@
+host = '__YOUR_MQTT_SERVER__';
+port = 34878; // CHANGE YOUR  WEBSOCKETS PORT (TLS ONLY)
+topic = '__YOUR_MQTT_TOPIC';        // topic to subscribe to
+useTLS = true; // DON'T CHANGE
+mqtt_username = "__YOUR_MQTT_USERNAME__";
+mqtt_password = "__YOUR_MQTT_PASSWORD__";
